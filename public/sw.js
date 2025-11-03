0a2f0247d6553409f1029e1c87289ab4f3154e30
Jmmichael19 <167400971+Jmmichael19@users.noreply.github.com>
2025-11-03 23:07:19 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,21 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installing...");
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+  console.log('🛠️ Service Worker: Installed');
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll(["/", "/index.html"]);
+    })
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated!");
+
+self.addEventListener('activate', (event) => {
+  console.log('🚀 Service Worker: Activated');
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).then((fetchResponse) => {
-          return fetchResponse;
-        })
-      );
+      return response || fetch(event.request);
     })
   );
 });
